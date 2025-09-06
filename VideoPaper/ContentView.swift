a5be557dd7454c92716bdc387aa504dc082a8ccb
Morris Richman <81453549+Mcrich23@users.noreply.github.com>
2025-09-06 13:13:02 -0700

    added WrappingHStack

--- VideoPaper/ContentView.swift
+++ VideoPaper/ContentView.swift
@@ -7,27 +7,41 @@
 
 import SwiftUI
 import SwiftData
+import WrappingHStack
 
 struct ContentView: View {
     @Environment(\.modelContext) private var modelContext
     @Query private var items: [SDWallpaperVideo]
     @State var jsonWallpaperCoordinator = JsonWallpaperCoordinator()
+    @State private var isPresentingInspector = true
 
     var body: some View {
         NavigationStack {
             ScrollView {
-                LazyVGrid(columns: [GridItem(.adaptive(minimum: 150))]) {
-                    ForEach(jsonWallpaperCoordinator.filteredAssets) { asset in
+                WrappingHStack(alignment: .leading, horizontalSpacing: 15, verticalSpacing: 15) {
+                    ForEach(jsonWallpaperCoordinator.assets) { asset in
                         if let thumbnailImage = asset.thumbnailImage {
                             Image(nsImage: thumbnailImage)
                                 .resizable()
                                 .aspectRatio(1, contentMode: .fill)
-                                .frame(minWidth: 150, maxWidth: .infinity, minHeight: 150, maxHeight: .infinity)
+                                .frame(width: 150, height: 150)
                                 .clipShape(RoundedRectangle(cornerRadius: 26))
                         }
                     }
                 }
                 .padding()
+            }
+            .inspector(isPresented: $isPresentingInspector, content: {
+                Text("Test")
+            })
+            .inspectorColumnWidth(ideal: 150)
+            .toolbar {
+                ToolbarItem(placement: .confirmationAction) {
+                    Button("\(isPresentingInspector ? "Hide" : "Show") Inspector", systemImage: "sidebar.right") {
+                        isPresentingInspector.toggle()
+                    }
+                    .labelStyle(.iconOnly)
+                }
             }
         }
         .onAppear {
